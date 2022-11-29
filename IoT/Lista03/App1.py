48d443b16713dc4e5ac8b1a9d2529860f6b5499b
lszwed5 <lukasz.szwed5@gmail.com>
2022-11-29 11:20:38 +0100

    add configuration via raw web app

--- IoT/Lista03/App1.py
+++ IoT/Lista03/App1.py
@@ -1,9 +1,15 @@
 from template import App
 from time import sleep
+import sys
 
 
 app = App()
-app.configure("configs/App_1_Config.json")
+if len(sys.argv) == 1:
+    print("No config dictionary given")
+    sys.exit(1)
+configuration = sys.argv[1]
+app.configure(configuration)
+# app.configure("configs/App_1_Config.json")
 frequency = app.settings['frequency']
 
 while True:
--- IoT/Lista03/server.py
+++ IoT/Lista03/server.py
@@ -1,14 +1,44 @@
-from flask import Flask, request, jsonify
+import sys
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import SelectField, SubmitField, StringField, IntegerField, FileField, BooleanField
+from wtforms.validators import InputRequired
 import json
-
+from subprocess import Popen
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+RECONFIG_PATH = "configs/reconfiguration.json"
+ACTIVE_GENERATORS = {}
+
+
+class ConfigureForm(FlaskForm):
+
+    generator_id = SelectField(u'Generator ID: ', choices=[1, 2, 3, 4, 5], coerce=int)
+
+    datasource = StringField('Source of data: ', validators=[InputRequired()])
+
+    protocol = SelectField('Protocol: ', choices=['HTTP', 'MQTT'])
+
+    target = StringField('Target URL: ', validators=[InputRequired()])
+
+    mqtt_topic = StringField('MQTT Topic: ')
+
+    frequency = IntegerField('Frequency of sending in seconds: ', default=0)
+
+    is_active = BooleanField('Active: ')
+
+    submit = SubmitField('Apply')
+
+
+def rerun_generator_app(app_path, config_path):
+    return Popen([sys.executable, app_path, config_path])
 
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return redirect(url_for('configure'))
 
 
 @app.route("/fileupload/<src>", methods=["POST"])
@@ -36,5 +66,47 @@ def getfile(file_nr):
     return jsonify(data)
 
 
+@app.route("/configuration/", methods=['GET', 'POST'])
+def configure():
+    form = ConfigureForm()
+
+    if form.validate_on_submit():
+        configuration = {
+            "App_id": form.generator_id.data,
+            "datasource": form.datasource.data,
+            "protocol": form.protocol.data,
+            "target": form.target.data,
+            "MQTT_topic": form.mqtt_topic.data,
+            "frequency": form.frequency.data,
+            "is_active": form.is_active.data
+        }
+        # print(configuration)
+
+        generator_name = f'App{configuration["App_id"]}.py'
+
+        with open(RECONFIG_PATH, "w", encoding='utf-8') as f:
+            json.dump(configuration, f, indent=4)
+
+        if configuration["is_active"]:
+            if str(configuration["App_id"]) in ACTIVE_GENERATORS.keys():
+                ACTIVE_GENERATORS[str(configuration["App_id"])].kill()
+                ACTIVE_GENERATORS[str(configuration["App_id"])] = rerun_generator_app(generator_name, RECONFIG_PATH)
+            else:
+                ACTIVE_GENERATORS[str(configuration["App_id"])] = rerun_generator_app(generator_name, RECONFIG_PATH)
+        else:
+            if str(configuration["App_id"]) in ACTIVE_GENERATORS.keys():
+                ACTIVE_GENERATORS[str(configuration["App_id"])].kill()
+
+        return redirect(url_for('successful'))
+
+    # print(form.errors)
+    return render_template('configure.html', form=form)
+
+
+@app.route('/configuration/success/')
+def successful():
+    return render_template('successful_configuration.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
